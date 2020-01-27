80c73e9c9c4c9bee51066e6552d605ea6e847c30
Ezio <assassinste@gmail.com>
2020-01-27 10:13:15 +0800

    Added 1st app 'main'

--- booktime/settings.py
+++ booktime/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # my own
+    "main.apps.MainConfig",
     # dev
     "django_extensions",
 ]
