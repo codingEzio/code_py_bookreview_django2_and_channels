95f45f0af8e6f6ff788a6b52b8f44ef09cdeb9dc
Ezio <assassinste@gmail.com>
2020-01-30 08:52:13 +0800

    Test whether 'contact-us' page is accessible or not

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,15 +1,24 @@
 from django.test import TestCase
+from main import forms
+from django.urls import reverse
 
 
 class TestPage(TestCase):
     def test_home_page_works(self):
-        response = self.client.get(path="/")
+        response = self.client.get(path=reverse("main:index"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
         self.assertContains(response, "Booktime")
 
     def test_about_us_page_works(self):
-        response = self.client.get(path="/about-us/")
+        response = self.client.get(path=reverse("main:about_us"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "about_us.html")
         self.assertContains(response, "Booktime")
+
+    def test_contact_us_page_works(self):
+        response = self.client.get(path=reverse("main:contact_us"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "contact_form.html")
+        self.assertContains(response, "Booktime")
+        self.assertIsInstance(response.context["form"], forms.ContactForm)
