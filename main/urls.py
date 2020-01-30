bc156a916743fa6d4374ab50c57f28d7e472bc3f
Ezio <assassinste@gmail.com>
2020-01-30 08:50:11 +0800

    Add view & routes for 'contact-us' page

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from main import views
+
 app_name = "main"
 
 urlpatterns = [
+    path(
+        route="contact-us/",
+        view=views.ContactUsView.as_view(),
+        name="contact_us",
+    ),
     path(
         route="about-us/",
         view=TemplateView.as_view(template_name="about_us.html"),
