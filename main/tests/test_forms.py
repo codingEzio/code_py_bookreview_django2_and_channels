3bde6e6bba099ce0fcf1341c54bbe6f12eb40c1d
Ezio <assassinste@gmail.com>
2020-02-02 16:56:43 +0800

    Test whether the email would be sent after registering

--- main/tests/test_forms.py
+++ main/tests/test_forms.py
@@ -6,7 +6,9 @@ from main import forms
 
 class TestForm(TestCase):
     TEST_SUBJECT = "Site message"
+    TEST_SUBJECT_REGISTRATION = "Welcome to Booktime"
     TEST_MESSAGE = "Hi there"
+    TEST_PASSWORD = "abcabcabc"
 
     def test_valid_contact_us_from_sends_email(self):
         form = forms.ContactForm(
@@ -27,3 +29,23 @@ class TestForm(TestCase):
         form = forms.ContactForm({"message": self.TEST_MESSAGE})
 
         self.assertFalse(form.is_valid())
+
+    def test_valid_signup_form_sends_email(self):
+        form = forms.UserCreationForm(
+            {
+                "email": "guest@booktime.com",
+                "password1": self.TEST_PASSWORD,
+                "password2": self.TEST_PASSWORD,
+            }
+        )
+
+        self.assertTrue(form.is_valid())
+
+        with self.assertLogs(logger="main.forms", level="INFO") as m:
+            form.send_mail()
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(
+            mail.outbox[0].subject, self.TEST_SUBJECT_REGISTRATION
+        )
+        self.assertGreaterEqual(len(m.output), 1)
