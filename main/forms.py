e3614bbda57db9019c6d816f659f7fc8ca5de0aa
Ezio <assassinste@gmail.com>
2020-02-02 16:18:08 +0800

    Customized UserCreationForm

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,14 @@
-from django import forms
-from django.core.mail import send_mail
-
 import logging
 from os import getenv
+
+from django import forms
+from django.core.mail import send_mail
+from django.contrib.auth.forms import (
+    UserCreationForm as DjangoUserCreationForm,
+    UsernameField,
+)
+
+from . import models
 
 
 logger = logging.getLogger(__name__)
@@ -26,4 +32,29 @@ class ContactForm(forms.Form):
             from_email=getenv("EMAIL_ADMIN"),
             recipient_list=[getenv("EMAIL_CUSTOMER_SERVICE")],
             fail_silently=False,
+        )
+
+
+class UserCreationForm(DjangoUserCreationForm):
+    class Meta(DjangoUserCreationForm.Meta):
+        """
+        This inner Meta class needs to be overriden when there is a custom User
+        model implemented (quote).
+        """
+        model = models.User
+        fields = ("email",)
+        field_classes = {"email": UsernameField}
+
+    def send_mail(self):
+        logger.info(
+            f"Sending signup email for email={self.cleaned_data['email']}"
+        )
+
+        message = f"Welcome {self.cleaned_data['email']}"
+        send_mail(
+            subject="Welcome to Booktime",
+            message=message,
+            from_email=getenv("EMAIL_ADMIN"),
+            recipient_list=[self.cleaned_data["email"]],
+            fail_silently=True,
         )
