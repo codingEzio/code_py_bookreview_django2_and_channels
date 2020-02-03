bc1e1e251f97450c12638d99312600a6848aadf4
Ezio <assassinste@gmail.com>
2020-02-03 20:40:52 +0800

    Fixing middleware implementation (there're still issues)

--- main/middlewares.py
+++ main/middlewares.py
@@ -1,18 +1,50 @@
+import logging
 from . import models
 
-__doc__ = """Modify
-- requests as they come in to the views     (view)
-- responses as they come out of the views   (template)"""
+logger = logging.getLogger(__name__)
 
 
 def basket_middleware(get_response):
+    """
+    The original implementation provided in the book was incomplete, here's an
+    improved version which is written by 'Spartak-Belov-Floresku' (the link:
+    https://github.com/Spartak-Belov-Floresku/practical-django2-and-channels2).
+
+    I did found that there were still some issues.
+    #TODO Make sure there wasn't any issues.
+    """
+
     def middleware(request):
+        user = request.user
+
         if "basket_id" in request.session:
             basket_id = request.session["basket_id"]
-            basket = models.Basket.objects.get(id=basket_id)
-            request.basket = basket
+
+            try:
+                basket = models.Basket.objects.get(id=basket_id)
+                request.basket = basket
+            except Exception as err:
+                logger.error(f"Error: {err}\n")
+
+                try:
+                    basket = models.Basket.objects.get(
+                        user=user, status=models.Basket.OPEN
+                    )
+                    request.basket = basket
+                except Exception as err:
+                    logger.error(f"Error: {err}\n")
+
+                    del request.session["basket_id"]
         else:
-            request.basket = None
+            try:
+                basket = models.Basket.objects.get(
+                    user=user, status=models.Basket.OPEN
+                )
+                request.basket = basket
+            except Exception as err:
+                logger.error(f"Error: {err}\n")
+
+                request.basket = None
 
         response = get_response(request)
 
