3637c1b1daccce994785091e274474048edc8d49
Ezio <assassinste@gmail.com>
2020-02-03 20:36:47 +0800

    Merge gst

--- main/signals.py
+++ main/signals.py
@@ -5,8 +5,9 @@ from PIL import Image
 from django.core.files.base import ContentFile
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in
 
-from .models import ProductImage
+from .models import ProductImage, Basket
 
 THUMBNAIL_SIZE = (300, 300)
 
@@ -33,3 +34,28 @@ def generate_thumbnail(sender, instance, **kwargs):
         thumbnail_name, ContentFile(temp_thumbnail.read()), save=False
     )
     temp_thumbnail.close()
+
+
+@receiver(signal=user_logged_in)
+def merge_baskets_if_found(sender, user, request, **kwargs):
+    """
+    Part of the job was done by our own middleware (basket_middleware).
+    """
+    anonymous_basket = getattr(request, "basket", None)
+    if anonymous_basket:
+        try:
+            loggedin_basket = Basket.objects.get(user=user, status=Basket.OPEN)
+
+            for basket_line in anonymous_basket.basketline_set.all():
+                basket_line.basket = loggedin_basket
+                basket_line.save()
+
+            anonymous_basket.delete()
+            request.basket = loggedin_basket
+
+            logger.info(f"Merged basket to id {loggedin_basket.id}")
+        except Basket.DoesNotExist:
+            anonymous_basket.user = user
+            anonymous_basket.save()
+
+            logger.info(f"Assigned user to basket id {anonymous_basket.id}")
