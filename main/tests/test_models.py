a6f9292a8b8cf12eacb6f6763a13780994980a6e
Ezio <assassinste@gmail.com>
2020-02-07 10:34:39 +0800

    Decouple the test data

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -5,15 +5,30 @@ from main import models
 
 
 class TestModel(TestCase):
+    EMAIL_SUFFIX = "@booktime.com"
+    TEST_SIGNUP_EMAIL = f"guest{EMAIL_SUFFIX}"
+    TEST_SIGNUP_PASSWORD = "abcabcabc"
+    TEST_PRODUCT_ONE = {
+        "name": "Purple Rose Hand Embroidery",
+        "price": Decimal(23.16),
+    }
+    TEST_PRODUCT_TWO = {
+        "name": "Lace Fabric Dress",
+        "price": Decimal(9.99),
+    }
+    TEST_PRODUCT_THREE = {
+        "name": "Blue Wedding Dress",
+        "price": Decimal(396.00),
+    }
+
     def test_active_manager_works(self):
+        models.Product.objects.create(**self.TEST_PRODUCT_ONE)
+        models.Product.objects.create(**self.TEST_PRODUCT_TWO)
         models.Product.objects.create(
-            name="Purple Rose Hand Embroidery", price=Decimal(23.16),
-        )
-        models.Product.objects.create(
-            name="Lace Fabric Dress", price=Decimal(9.99),
-        )
-        models.Product.objects.create(
-            name="Blue Wedding Dress", price=Decimal(396.00), active=False,
+            **self.TEST_PRODUCT_THREE, active=False,
         )
 
         self.assertEqual(len(models.Product.objects.active()), 2)
+
+    def test_create_order_works(self):
+        pass
