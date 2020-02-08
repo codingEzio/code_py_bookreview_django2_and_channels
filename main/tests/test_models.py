6b5da26584b83d42c25ecec1f9a774847b6e70b3
Ezio <assassinste@gmail.com>
2020-02-08 17:34:25 +0800

    Put 'factory-boy' into uses

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -2,62 +2,22 @@ from decimal import Decimal
 from django.test import TestCase
 
 from main import models
+from main import factories
 
 
 class TestModel(TestCase):
-    EMAIL_SUFFIX = "@booktime.com"
-    TEST_USER = {
-        "NAME": "guest",
-        "TEST_SIGNUP_EMAIL": f"guest{EMAIL_SUFFIX}",
-        "TEST_SIGNUP_PASSWORD": "abcabcabc",
-    }
-    TEST_PRODUCT_ONE = {
-        "name": "Purple Rose Hand Embroidery",
-        "price": Decimal(23.16),
-    }
-    TEST_PRODUCT_TWO = {
-        "name": "Lace Fabric Dress",
-        "price": Decimal(9.99),
-    }
-    TEST_PRODUCT_THREE = {
-        "name": "Blue Wedding Dress",
-        "price": Decimal(396.00),
-    }
-    TEST_ADDRESS_SHIPPING = "Edinburgh EH99 1SP, United Kingdom"
-    TEST_ADDRESS_BILLING = TEST_ADDRESS_SHIPPING
-    TEST_COUNTRY = "uk"
-    TEST_CITY = "Edinburgh"
-
     def test_active_manager_works(self):
-        models.Product.objects.create(**self.TEST_PRODUCT_ONE)
-        models.Product.objects.create(**self.TEST_PRODUCT_TWO)
-        models.Product.objects.create(
-            **self.TEST_PRODUCT_THREE, active=False,
-        )
+        factories.ProductFactory.create_batch(2, active=True)
+        factories.ProductFactory(active=False)
 
         self.assertEqual(len(models.Product.objects.active()), 2)
 
     def test_create_order_works(self):
-        prod1 = models.Product.objects.create(**self.TEST_PRODUCT_ONE)
-        prod2 = models.Product.objects.create(**self.TEST_PRODUCT_TWO)
-        user1 = models.User.objects.create_user(
-            self.TEST_USER["TEST_SIGNUP_EMAIL"],
-            self.TEST_USER["TEST_SIGNUP_PASSWORD"],
-        )
-        billing_addr = models.Address.objects.create(
-            user=user1,
-            name=self.TEST_USER["NAME"],
-            address1=self.TEST_ADDRESS_BILLING,
-            city=self.TEST_CITY,
-            country=self.TEST_COUNTRY,
-        )
-        shipping_addr = models.Address.objects.create(
-            user=user1,
-            name=self.TEST_USER["NAME"],
-            address1=self.TEST_ADDRESS_SHIPPING,
-            city=self.TEST_CITY,
-            country=self.TEST_COUNTRY,
-        )
+        prod1 = factories.ProductFactory()
+        prod2 = factories.ProductFactory()
+        user1 = factories.UserFactory()
+        billing_addr = factories.AddressFactory(user=user1)
+        shipping_addr = factories.AddressFactory(user=user1)
 
         basket = models.Basket.objects.create(user=user1)
         models.BasketLine.objects.create(basket=basket, product=prod1)
@@ -73,8 +33,8 @@ class TestModel(TestCase):
         order.refresh_from_db()
 
         self.assertEquals(order.user, user1)
-        self.assertEquals(order.billing_address1, self.TEST_ADDRESS_BILLING)
-        self.assertEquals(order.shipping_address1, self.TEST_ADDRESS_SHIPPING)
+        self.assertEquals(order.billing_address1, billing_addr.address1)
+        self.assertEquals(order.shipping_address1, shipping_addr.address1)
 
         self.assertEquals(order.lines.all().count(), 2)
 
