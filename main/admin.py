bcd378aebdfdf7bf93e95d919701c345d2a6173c
Ezio <assassinste@gmail.com>
2020-02-10 18:41:58 +0800

    Customizing admin, heavily (different roles, different content)

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,20 @@
+from datetime import datetime, timedelta
+import logging
+
 from django.contrib import admin
 from django.utils.html import format_html
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.db.models.functions import TruncDay
+from django.db.models import Avg, Count, Min, Sum  # noqa
+from django.urls import path
+from django.template.response import TemplateResponse
 
 from . import models
 
 
-@admin.register(models.User)
+logger = logging.getLogger(name=__name__)
+
+
 class UserAdmin(DjangoUserAdmin):
     fieldsets = (
         (None, {"fields": ("email", "password")}),
@@ -46,6 +55,34 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ("in_stock",)
     search_fields = ("name",)
     prepopulated_fields = {"slug": ("name",)}
+    autocomplete_fields = ("tags",)
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        About the field 'slug':
+          it's an important field for our site, it is used in all the product
+          URLs. We want to limit the ability to change this ONLY TO the owners
+          of the company.
+        """
+        if request.user.is_superuser:
+            return self.readonly_fields
+
+        return list(self.readonly_fields) + ["slug", "name"]
+
+    def get_prepopulated_fields(self, request, obj=None):
+        """
+        Required for 'get_readonly_fields' to work.
+        """
+        if request.user.is_superuser:
+            return self.prepopulated_fields
+        else:
+            return {}
+
+
+class DispatchersProductAdmin(ProductAdmin):
+    readonly_fields = ("description", "price", "tags", "active")
+    prepopulated_fields = {}
+    autocomplete_fields = ()
 
 
 class ProductTagAdmin(admin.ModelAdmin):
@@ -56,6 +93,18 @@ class ProductTagAdmin(admin.ModelAdmin):
     list_filter = ("active",)
     search_fields = ("name",)
     prepopulated_fields = {"slug": ("name",)}
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.readonly_fields
+
+        return list(self.readonly_fields) + ["slug", "name"]
+
+    def get_prepopulated_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.prepopulated_fields
+        else:
+            return {}
 
 
 class ProductImageAdmin(admin.ModelAdmin):
@@ -133,9 +182,239 @@ class OrderAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(models.Product, ProductAdmin)
-admin.site.register(models.ProductTag, ProductTagAdmin)
-admin.site.register(models.ProductImage, ProductImageAdmin)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "name",
+        "address1",
+        "address2",
+        "city",
+        "country",
+    )
+    readonly_fields = ("user",)
 
-admin.site.register(models.Basket, BasketAdmin)
-admin.site.register(models.Order, OrderAdmin)
+
+""" The role-specific classes """
+
+
+class CentralOfficeOrderLineInline(admin.TabularInline):
+    model = models.OrderLine
+    readonly_fields = ("product",)
+
+
+class CentralOfficeOrderAdmin(admin.ModelAdmin):
+    """
+    Employees (aka. Central Office) need a custom version of the `order` views
+    because they aren't allowed to change products that already purchased
+    without adding and removing lines.
+    """
+
+    list_display = (
+        "id",
+        "user",
+        "status",
+    )
+    list_editable = ("status",)
+    readonly_fields = ("user",)
+    list_filter = ("status", "shipping_country", "date_added")
+    inlines = (CentralOfficeOrderLineInline,)
+
+    fieldsets = (
+        (None, {"fields": ("user", "status",)}),
+        (
+            "Billing info",
+            {
+                "fields": (
+                    "billing_name",
+                    "billing_address1",
+                    "billing_address2",
+                    "billing_postal_code",
+                    "billing_city",
+                    "billing_country",
+                )
+            },
+        ),
+        (
+            "Shipping info",
+            {
+                "fields": (
+                    "shipping_name",
+                    "shipping_address1",
+                    "shipping_address2",
+                    "shipping_postal_code",
+                    "shipping_city",
+                    "shipping_country",
+                )
+            },
+        ),
+    )
+
+
+class DispatchersOrderAdmin(admin.ModelAdmin):
+    """
+    Dispatchers do not need to see the billing address in the fields.
+    """
+
+    list_display = (
+        "id",
+        "shipping_name",
+        "date_added",
+        "status",
+    )
+    list_filter = ("status", "shipping_country", "date_added")
+    inlines = (CentralOfficeOrderLineInline,)
+    fieldsets = (
+        (
+            "Shipping info",
+            {
+                "fields": (
+                    "shipping_name",
+                    "shipping_address1",
+                    "shipping_address2",
+                    "shipping_postal_code",
+                    "shipping_city",
+                    "shipping_country",
+                )
+            },
+        ),
+    )
+
+    def get_queryset(self, request):
+        """
+        Dispatchers r only allowed to see orders that are ready to be shipped.
+        """
+        qs = super().get_queryset(request)
+        return qs.filter(status=models.Order.PAID)
+
+
+""" Classes that will be used multiple times (for different roles) """
+
+
+class ColoredAdminSite(admin.sites.AdminSite):
+    """
+    Pass a couple of values to the Django Admin templates (including our own).
+    """
+
+    def each_context(self, request):
+        context = super().each_context(request)
+
+        context["site_header_color"] = getattr(
+            object=self, name="site_header_color", default=None
+        )
+        context["module_caption_color"] = getattr(
+            object=self, name="module_caption_color", default=None
+        )
+
+        return context
+
+
+class ReportingColoredAdminSite(ColoredAdminSite):
+    """
+    Add reporting views to the list of available urls and will list them from
+    the index page (I don't quite understand what is "list from").
+    """
+
+    def get_urls(self):
+        urls = super().get_urls()
+
+        my_urls = [
+            path(
+                route="orders_per_day/",
+                view=self.admin_view(self.orders_per_day),
+            ),
+        ]
+
+        return my_urls + urls
+
+    def orders_per_day(self, request):
+        starting_day = datetime.now() - timedelta(days=180)
+
+        order_data = (
+            models.Order.objects.filter(date_added__gt=starting_day)
+            .annotate(day=TruncDay("date_added"))
+            .values("day")
+            .annotate(c=Count("id"))
+        )
+
+        labels = [x["day"].strftime("%Y-%m-%d") for x in order_data]
+        values = [x["c"] for x in order_data]
+
+        context = dict(
+            self.each_context(request),
+            title="Orders per day",
+            labels=labels,
+            values=values,
+        )
+
+        return TemplateResponse(
+            request=request, template="orders_per_day.html", context=context
+        )
+
+    def index(self, request, extra_context=None):
+        reporting_pages = [
+            {"name": "Orders per day", "link": "orders_per_day/"}
+        ]
+
+        if not extra_context:
+            extra_context = {}
+        extra_context = {"reporting_pages": reporting_pages}
+
+        return super().index(request, extra_context)
+
+
+""" Define the instances of AdminSite, each with their own perms and colors """
+
+
+class OwnersAdminSite(ReportingColoredAdminSite):
+    site_header = "Booktime owners administration"
+    site_header_color = "black"
+    module_caption_color = "grey"
+
+    def has_permission(self, request):
+        return request.user.is_active and request.user.is_superuser
+
+
+class CentralOfficeAdminSite(ReportingColoredAdminSite):
+    site_header = "Booktime central office administration"
+    site_header_color = "purple"
+    module_caption_color = "pink"
+
+    def has_permission(self, request):
+        return request.user.is_active and request.user.is_employee
+
+
+class DispatchersAdminSite(ReportingColoredAdminSite):
+    site_header = "Booktime central dispatch administration"
+    site_header_color = "green"
+    module_caption_color = "lightgreen"
+
+    def has_permission(self, request):
+        return request.user.is_active and request.user.is_dispatcher
+
+
+""" Register the admin sites """
+
+
+admin.site.register(models.User, UserAdmin)
+
+main_admin = OwnersAdminSite()
+central_office_admin = CentralOfficeAdminSite("central_office_admin")
+dispatchers_admin = DispatchersAdminSite("dispatchers-admin")
+
+main_admin.register(models.Product, ProductAdmin)
+main_admin.register(models.ProductTag, ProductTagAdmin)
+main_admin.register(models.ProductImage, ProductImageAdmin)
+main_admin.register(models.User, UserAdmin)
+main_admin.register(models.Address, AddressAdmin)
+main_admin.register(models.Basket, BasketAdmin)
+main_admin.register(models.Order, OrderAdmin)
+
+central_office_admin.register(models.Product, ProductAdmin)
+central_office_admin.register(models.ProductTag, ProductTagAdmin)
+central_office_admin.register(models.ProductImage, ProductImageAdmin)
+central_office_admin.register(models.Address, AddressAdmin)
+central_office_admin.register(models.Order, CentralOfficeOrderAdmin)
+
+dispatchers_admin.register(models.Product, DispatchersProductAdmin)
+dispatchers_admin.register(models.ProductTag, ProductTagAdmin)
+dispatchers_admin.register(models.Order, DispatchersOrderAdmin)
